f85e72340bd9a7c9eda32f398d7d461aade410a5
Tiejun Hu <87230907+gzhutiejun@users.noreply.github.com>
2025-07-28 17:38:11 +0800

    add route

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import "./assets/scss/styles.scss"
 import Blank from './pages/blank.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Blank></Blank>
+    <BrowserRouter> 
+      <Routes>
+        <Route path="/" element={<Blank />} />
+      </Routes>
+    </BrowserRouter>
+
   </StrictMode>,
 )
